e18529e7121026102a3bf30f77196fbd0f681e98
BeatrizCercal <biaccachoeira@gmail.com>
2026-09-02 23:42:07 -0300

    Update exercicio 04

---
+++ exercicio04.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int maiorValor(int v[], int n);
+
+int main(void)
+{
+    int n;
+
+    printf("Digite o tamanho do vetor: ");
+    scanf("%i", &n);
+
+    int v[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("Digite um numero: ");
+        scanf("%i", &v[i]);
+    }
+
+    printf("Maior valor do vetor: %i\n", maiorValor(v, n));
+
+    return 0;
+}
+
+int maiorValor(int v[], int n)
+{
+    if (n == 1)
+        return v[0];
+
+    if (v[n - 1] > maior(v, n - 1))
+        return v[n - 1];
+
+    return maior(v, n - 1);
+}
